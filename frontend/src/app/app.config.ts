3d808e35c1ab95b69c60872b4b4a0a426e7a4f9d
Ashukla-Portfolio <adi_shukla@outlook.com>
2026-04-27 17:26:27 -0400

    Rebuilt site with correct configuration. no file insertions

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,5 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withFetch())
-  ]
+  providers: [provideBrowserGlobalErrorListeners()],
 };
--- frontend/src/app/query/query.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-query',
-  imports: [],
-  templateUrl: './query.html',
-  styleUrl: './query.css',
-})
-export class Query {}
