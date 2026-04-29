5a4fc63b51d932217288a24420ab267d76043823
Ashukla-Portfolio <adi_shukla@outlook.com>
2026-04-29 16:16:14 -0400

    stable vercel CORS

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "https://hackathon-a26.vercel.app", "https://*.vercel.app"]
+    allow_origins=["http://localhost:4200", "https://hackathon-a26.vercel.app"]
     allow_methods=["POST", "GET"],
     allow_headers=["*"]
 )
