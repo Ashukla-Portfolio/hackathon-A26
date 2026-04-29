c032fc97d292ae6fec9caea0cdccd538cde59b10
Ashukla-Portfolio <adi_shukla@outlook.com>
2026-04-29 15:25:46 -0400

    set prod API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000'  // update this to your deployed backend URL before demo
+  apiUrl: 'https://hackathon-a26.onrender.com'
 };
